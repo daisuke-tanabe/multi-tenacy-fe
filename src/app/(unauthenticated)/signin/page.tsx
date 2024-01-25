5c91a8e114941090afefb155c10ef57bfa941d03
daisuke-tanabe <dev.daisuke.tanabe@gmail.com>
2024-01-25 17:58:06 +0900

    feat: 多要素認証の対応

--- src/app/(unauthenticated)/signin/page.tsx
+++ src/app/(unauthenticated)/signin/page.tsx
@@ -2,8 +2,21 @@
 
 import { useRouter } from 'next/navigation';
 import {ChangeEvent, FormEvent, useState} from "react";
+import {QRCodeSVG} from 'qrcode.react';
 
-import {fetchWithHandling} from "@/lib";
+import {fetchWithHandling, isErrorResponse} from "@/lib";
+
+type ChallengeName = 'MFA_SETUP' | 'SOFTWARE_TOKEN_MFA';
+
+type ResBody = {
+  id: string;
+};
+
+type NextStepResBody = {
+  challengeName: ChallengeName;
+  qrCodeUrl?: string;
+  session?: string;
+};
 
 export default function Page() {
   const router = useRouter();
@@ -12,18 +25,23 @@ export default function Page() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const [challengeName, setChallengeName] = useState<ChallengeName | null>(null);
+  const [qrCodeUrl, setQrCodeUrl] = useState<string | undefined>(undefined);
+  const [mfaCode, setMfaCode] = useState<string>('');
+  const [challengeSession, setChallengeSession] = useState<string | undefined>(undefined);
+
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.name === 'username') setTenantId(event.target.value);
     if (event.target.name === 'email') setEmail(event.target.value);
     if (event.target.name === 'password') setPassword(event.target.value);
+    if (event.target.name === 'mfacode') setMfaCode(event.target.value);
   };
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log('postで実行したいんだが')
 
     try {
-      await fetchWithHandling(`${process.env.NEXT_PUBLIC_API_URL}/auth/signin`, {
+      const response = await fetchWithHandling<ResBody | NextStepResBody>(`${process.env.NEXT_PUBLIC_API_URL}/auth/signin`, {
         headers: {
           'Content-Type': 'application/json',
         },
@@ -33,9 +51,30 @@ export default function Page() {
         body: JSON.stringify({
           tenantId,
           email,
-          password
+          password,
+          ...(challengeName　&& { nextStep: challengeName }),
+          ...(mfaCode && { mfaCode }),
+          ...(challengeSession && { session: challengeSession })
         })
       });
+
+      if (isErrorResponse(response)) {
+        console.log(response.error.message);
+        return;
+      }
+
+      if ('challengeName' in response && response.challengeName === 'MFA_SETUP') {
+        setChallengeName(response.challengeName);
+        setQrCodeUrl(response.qrCodeUrl);
+        setChallengeSession(response.session);
+        return;
+      }
+
+      if ('challengeName' in response && response.challengeName === 'SOFTWARE_TOKEN_MFA') {
+        setChallengeName(response.challengeName);
+        return;
+      }
+
       const url = new URL(decodeURIComponent(document.location.href));
       const callbackUrl = url.searchParams.get('callbackUrl') || '/';
       router.push(callbackUrl);
@@ -48,15 +87,40 @@ export default function Page() {
     <div>
       <h1>signin</h1>
       <form onSubmit={handleSubmit}>
-        <div>
-          tenant_id: <input type="text" name="username" value={tenantId} onChange={handleChange} />
-        </div>
-        <div>
-          email: <input type="text" name="email" value={email} onChange={handleChange} />
-        </div>
-        <div>
-          password: <input type="password" name="password" value={password} onChange={handleChange} />
-        </div>
+        { challengeName !== 'MFA_SETUP' && challengeName !== 'SOFTWARE_TOKEN_MFA' && (
+          <>
+            <div>
+              tenant_id: <input type="text" name="username" value={tenantId} onChange={handleChange} />
+            </div>
+            <div>
+              email: <input type="text" name="email" value={email} onChange={handleChange} />
+            </div>
+            <div>
+              password: <input type="password" name="password" value={password} onChange={handleChange} />
+            </div>
+          </>
+        )
+      }
+
+        {
+          challengeName === 'MFA_SETUP' && qrCodeUrl && (
+            <div>
+              <QRCodeSVG value={qrCodeUrl} />
+              <div>
+                mfa_code: <input type="text" name="mfacode" value={mfaCode} onChange={handleChange} />
+              </div>
+            </div>
+          )
+        }
+
+        {
+          challengeName === 'SOFTWARE_TOKEN_MFA' && (
+            <div>
+              mfa_code: <input type="text" name="mfacode" value={mfaCode} onChange={handleChange} />
+            </div>
+          )
+        }
+
         <div>
           <button type="submit" value="submit">Submit</button>
         </div>
